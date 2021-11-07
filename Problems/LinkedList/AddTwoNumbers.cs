0527a39fff1e391e8820ecc894b360a459a9f50b
stain <stain@DESKTOP-UEMELPH>
2021-11-07 12:01:26 +0700

    Add project files.

---
+++ Problems/LinkedList/AddTwoNumbers.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using NUnit.Framework;
+
+namespace Problems.LinkedList
+{
+    [TestFixture]
+    public sealed class AddTwoNumbers
+    {
+        private static readonly ListNode q1 = new ListNode(3, new ListNode(4, new ListNode(2)));
+        private static readonly ListNode q2 = new ListNode(3, new ListNode(4, new ListNode(2)));
+
+        private static readonly List<ListNode> q = new List<ListNode> {q1, q2};
+
+        private static string FileName = nameof(AddTwoNumbers);
+        private StreamReader reader;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            var namespaceName = typeof(AddTwoNumbers).Namespace;
+            this.reader = new StreamReader($"{namespaceName.Split(".")[1]}\\TestData\\{FileName}.txt");
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            this.reader.Dispose();
+        }
+
+        [Test]
+        public void Test()
+        {
+            var actualResult = AddTwoNumbers1();
+        }
+
+        public ListNode AddTwoNumbers1()
+        {
+            var l11 = new ListNode(3);
+            var l12 = new ListNode(4, l11);
+            var l13 = new ListNode(2, l12);
+
+            var l21 = new ListNode(4);
+            var l22 = new ListNode(6, l21);
+            var l23 = new ListNode(5, l22);
+
+            var l1 = l13;
+            var l2 = l23;
+
+            ListNode l = null;
+            ListNode iterrator = null;
+            var add = 0;
+            while (l1 != null && l2 != null)
+            {
+                var value = l1.val + l2.val + add;
+                add = value / 10;
+                value %= 10;
+
+                var node = new ListNode(value);
+
+                if (l == null)
+                {
+                    l = node;
+                    iterrator = node;
+                }
+                else
+                {
+                    iterrator.next = node;
+                    iterrator = node;
+                }
+
+                l1 = l1.next;
+                l2 = l2.next;
+            }
+
+            if (l1 == null)
+            {
+                l1 = l2;
+            }
+
+            while (l1 != null)
+            {
+                var value = l1.val + add;
+                add = value / 10;
+                value %= 10;
+
+                var node = new ListNode(value);
+
+                iterrator.next = node;
+                iterrator = node;
+
+                l1 = l1.next;
+            }
+
+            if (add > 0)
+            {
+                var node = new ListNode(add);
+                iterrator.next = node;
+            }
+
+            return l;
+        }
+
+        public class ListNode
+        {
+            public int val;
+
+            public ListNode next;
+
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+    }
+}
---
+++ Problems/String/LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Problems.String
+{
+    [TestFixture]
+    public sealed class LongestSubstringWithoutRepeatingCharacters
+    {
+        [Test]
+        public void Test()
+        {
+            var s = "";
+
+            var set = new HashSet<char>(s.Length);
+
+            var left = 0;
+            var right = 0;
+            var maxLength = 0;
+
+            while (left < s.Length)
+            {
+                if (!set.Contains(s[left]))
+                {
+                    set.Add(s[left]);
+                    left++;
+                    continue;
+                }
+
+                maxLength = Math.Max(maxLength, left - right);
+
+                while (s[right] != s[left])
+                {
+                    set.Remove(s[right]);
+                    right++;
+                }
+
+                left++;
+                right++;
+            }
+
+            maxLength = Math.Max(maxLength, left - right);
+        }
+    }
+}
