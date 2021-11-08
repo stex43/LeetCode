6f6643326d6c24a10a1c9f29ff53ed700735bcc6
stex43 <stainy@yandex.ru>
2021-11-08 23:11:59 +0700

    - LongestSubstringWithoutRepeatingCharacters adjustments

--- Problems/String/LongestSubstringWithoutRepeatingCharacters.cs
+++ Problems/String/LongestSubstringWithoutRepeatingCharacters.cs
@@ -7,39 +7,91 @@ namespace Problems.String
     [TestFixture]
     public sealed class LongestSubstringWithoutRepeatingCharacters
     {
-        [Test]
-        public void Test()
+        private static IEnumerable<TestCaseData> TestCases()
         {
-            var s = "";
+            yield return new TestCaseData("abcabcbb")
+                .SetName("{a} {m}")
+                .Returns(3);
 
+            yield return new TestCaseData("bbbbb")
+                .SetName("{a} {m}")
+                .Returns(1); ;
+
+            yield return new TestCaseData("pwwkew")
+                .SetName("{a} {m}")
+                .Returns(3); ;
+
+            yield return new TestCaseData("")
+                .SetName("{a} {m}")
+                .Returns(0); ;
+
+            yield return new TestCaseData("abcde")
+                .SetName("{a} {m}")
+                .Returns(5); ;
+        }
+
+        [TestCaseSource(nameof(TestCases))]
+        public int Mine_First(string s)
+        {
             var set = new HashSet<char>(s.Length);
 
             var left = 0;
             var right = 0;
             var maxLength = 0;
 
-            while (left < s.Length)
+            while (right < s.Length)
             {
-                if (!set.Contains(s[left]))
+                if (!set.Contains(s[right]))
                 {
-                    set.Add(s[left]);
-                    left++;
+                    set.Add(s[right]);
+                    right++;
                     continue;
                 }
 
-                maxLength = Math.Max(maxLength, left - right);
+                maxLength = Math.Max(maxLength, right - left);
 
-                while (s[right] != s[left])
+                while (s[left] != s[right])
                 {
-                    set.Remove(s[right]);
-                    right++;
+                    set.Remove(s[left]);
+                    left++;
                 }
 
+                right++;
                 left++;
+            }
+
+            return Math.Max(maxLength, right - left);
+        }
+
+        [TestCaseSource(nameof(TestCases))]
+        public int Mine_Second(string s)
+        {
+            var chars = new Dictionary<char, int>();
+
+            var left = 0;
+            var right = 0;
+            var maxLength = 0;
+
+            while (right < s.Length)
+            {
+                var currentChar = s[right];
+
+                if (!chars.ContainsKey(currentChar))
+                {
+                    chars[currentChar] = right;
+                    right++;
+                    continue;
+                }
+
+                maxLength = Math.Max(maxLength, right - left);
+
+                left = chars[currentChar] + 1;
+                chars[currentChar] = right;
+
                 right++;
             }
 
-            maxLength = Math.Max(maxLength, left - right);
+            return Math.Max(maxLength, right - left);
         }
     }
 }
