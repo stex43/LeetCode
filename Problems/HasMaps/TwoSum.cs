ad7b5a530c4f00060fe8746e3d3acd02e2bc96f7
kreslina.a <kreslina.a@skbkontur.ru>
2021-11-09 23:29:44 +0700

    done - TwoSum

---
+++ Problems/HasMaps/TwoSum.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Problems.HasMaps
+{
+    [TestFixture]
+    public sealed class TwoSum
+    {
+        private static IEnumerable<TestCaseData> TestCases()
+        {
+            yield return new TestCaseData(new[] { 2, 7, 11, 15 }, 9)
+                .SetName("{a} {m}")
+                .Returns(new[] { 0, 1 });
+
+            yield return new TestCaseData(new[] { 3, 2, 4 }, 6)
+                .SetName("{a} {m}")
+                .Returns(new[] { 1, 2 });
+
+            yield return new TestCaseData(new[] { 3, 3 }, 6)
+                .SetName("{a} {m}")
+                .Returns(new[] { 0, 1 });
+        }
+
+        [TestCaseSource(nameof(TestCases))]
+        public int[] Mine_First(int[] nums, int target)
+        {
+            var map = new Dictionary<int, int>(nums.Length);
+            for (var i = 0; i < nums.Length; i++)
+            {
+                var num = nums[i];
+
+                if (map.TryGetValue(num, out var j))
+                {
+                    return new[] { j, i };
+                }
+
+                map[target - num] = i;
+            }
+
+            return null;
+        }
+
+        [TestCaseSource(nameof(TestCases))]
+        public int[] Mine_BruteForce(int[] nums, int target)
+        {
+            for (var i = 0; i < nums.Length; i++)
+            {
+                for (var j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new[] {i, j};
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Problems/Template.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Problems
+{
+    [TestFixture]
+    public sealed class Template
+    {
+        private static IEnumerable<TestCaseData> TestCases()
+        {
+            yield return new TestCaseData()
+                .SetName("{a} {m}")
+                .Returns(0);
+        }
+
+        [TestCaseSource(nameof(TestCases))]
+        public int Mine_First()
+        {
+            return 0;
+        }
+    }
+}
