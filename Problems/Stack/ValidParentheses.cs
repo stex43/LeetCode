7bedd6337da0deef68b3027fb3f77e9f89cc85d2
kreslina.a <kreslina.a@skbkontur.ru>
2021-11-14 17:54:29 +0700

    start - ValidParentheses

---
+++ Problems/Stack/ValidParentheses.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Problems.Stack
+{
+    [TestFixture]
+    public sealed class ValidParentheses
+    {
+        private static IEnumerable<TestCaseData> TestCases()
+        {
+            yield return new TestCaseData("()")
+                .SetName("{a} {m}")
+                .Returns(true);
+
+            yield return new TestCaseData("()[]{}")
+                .SetName("{a} {m}")
+                .Returns(true);
+
+            yield return new TestCaseData("(]")
+                .SetName("{a} {m}")
+                .Returns(false);
+
+            yield return new TestCaseData("([)]")
+                .SetName("{a} {m}")
+                .Returns(false);
+
+            yield return new TestCaseData("{[]}")
+                .SetName("{a} {m}")
+                .Returns(false);
+        }
+
+        [TestCaseSource(nameof(TestCases))]
+        public bool Mine_First(string s)
+        {
+            var openings = new HashSet<char>(new[] { '(', '[', '{' });
+            var closings = new HashSet<char>(new[] { ')', ']', '}' });
+
+            var stack = new Stack<char>(s.Length);
+            foreach (var symbol in s)
+            {
+
+            }
+            return false;
+        }
+    }
+}
