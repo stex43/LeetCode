9312f02a52ea305e247abe6e52f0df6d8aea9fe3
stex43 <stainy@yandex.ru>
2021-11-14 19:22:12 +0700

    done - ValidParentheses

--- Problems/Stack/ValidParentheses.cs
+++ Problems/Stack/ValidParentheses.cs
@@ -26,21 +26,51 @@ namespace Problems.Stack
 
             yield return new TestCaseData("{[]}")
                 .SetName("{a} {m}")
+                .Returns(true);
+
+            yield return new TestCaseData("}")
+                .SetName("{a} {m}")
                 .Returns(false);
+
+            yield return new TestCaseData("(((((())))))")
+                .SetName("{a} {m}")
+                .Returns(true);
+
+            yield return new TestCaseData("(((((((()")
+                .SetName("{a} {m}")
+                .Returns(false);
+
+            yield return new TestCaseData("((()(())))")
+                .SetName("{a} {m}")
+                .Returns(true);
         }
 
         [TestCaseSource(nameof(TestCases))]
         public bool Mine_First(string s)
         {
-            var openings = new HashSet<char>(new[] { '(', '[', '{' });
-            var closings = new HashSet<char>(new[] { ')', ']', '}' });
+            var parenthesesMap = new Dictionary<char, char>
+            {
+                { '(', ')' },
+                { '[', ']' },
+                { '{', '}' }
+            };
 
             var stack = new Stack<char>(s.Length);
             foreach (var symbol in s)
             {
+                if (parenthesesMap.ContainsKey(symbol))
+                {
+                    stack.Push(symbol);
+                    continue;
+                }
 
+                if (!stack.TryPop(out var peek) || symbol != parenthesesMap[peek])
+                {
+                    return false;
+                }
             }
-            return false;
+
+            return stack.Count == 0;
         }
     }
 }
